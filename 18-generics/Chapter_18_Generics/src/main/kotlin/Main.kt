5a6923f7c97fab528593c389f0683119fd8097b4
PauloLajos <paulolajos@gmail.com>
2023-11-14 15:37:17 +0100

    18. Generics - Creating your own generic constraints

--- 18-generics/Chapter_18_Generics/src/main/kotlin/Main.kt
+++ 18-generics/Chapter_18_Generics/src/main/kotlin/Main.kt
@@ -7,13 +7,79 @@ fun <T> List<T>.toBulletedList(): String {
     separator, postfix = "\n")
 }
 
+// 1
+class Mover<T>(
+// 2
+    thingsToMove: List<T>,
+    val truckHeightInInches: Int = (12 * 12)
+) {
+    // 3
+    private var thingsLeftInOldPlace = mutableListOf<T>()
+    private var thingsInTruck = mutableListOf<T>()
+    private var thingsInNewPlace = mutableListOf<T>()
+    // 4
+    init {
+        thingsLeftInOldPlace.addAll(thingsToMove)
+    }
+    // 5
+    /*fun moveEverythingToTruck() {
+        while (thingsLeftInOldPlace.count() > 0) {
+            val item = thingsLeftInOldPlace.removeAt(0)
+            thingsInTruck.add(item)
+            println("Moved your $item to the truck!")
+        }
+    }*/
+    fun moveEverythingToTruck() {
+        while (thingsLeftInOldPlace.count() > 0) {
+            val item = thingsLeftInOldPlace.removeAt(0)
+            if (item is BreakableThing) {
+                if (!item.isBroken) {
+                    thingsInTruck.add(item)
+                    println("Moved your $item to the truck!")
+                } else {
+                    println("Could not move your $item to the truck")
+                }
+            } else {
+                thingsInTruck.add(item)
+                println("Moved your $item to the truck!")
+            }
+        }
+    }
+    // 6
+    fun moveEverythingIntoNewPlace() {
+        while (thingsInTruck.count() > 0) {
+            val item = thingsInTruck.removeAt(0)
+            thingsInNewPlace.add(item)
+            println("Moved your $item into your new place!")
+        }
+    }
+    // 7
+    fun finishMove() {
+        println("OK, we finished! We were able to move your:${thingsInNewPlace.toBulletedList()}")
+    }
+}
+
+class CheapThing(val name: String) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+class BreakableThing(
+    val name: String,
+    var isBroken: Boolean = false
+) {
+    fun smash() {
+        isBroken = true
+    }
+    override fun toString(): String {
+        return name
+    }
+}
+
+
 //fun main(args: Array<String>) {
 fun main() {
-    //println("Hello World!")
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    //println("Program arguments: ${args.joinToString()}")
-
     //val names: List<String> = listOf("Bob", "Carol", "Ted", "Alice")
     val names = listOf("Bob", "Carol", "Ted", "Alice")
     println("Names: $names")
@@ -43,4 +109,28 @@ fun main() {
     println("Values for keys with E: ${valuesForKeysWithE.toBulletedList()}")
 
     println("Names: ${names.toBulletedList()}")
+
+    val cheapThings = listOf(
+        CheapThing("Cinder Block table"),
+        CheapThing("Box of old books"),
+        CheapThing("Ugly old couch")
+    )
+    val cheapMover = Mover(cheapThings)
+    cheapMover.moveEverythingToTruck()
+    cheapMover.moveEverythingIntoNewPlace()
+    cheapMover.finishMove()
+
+    val television = BreakableThing("Flat-Screen Television")
+    val breakableThings = listOf(
+        television,
+        BreakableThing("Mirror"),
+        BreakableThing("Guitar")
+    )
+    val expensiveMover = Mover(breakableThings)
+    expensiveMover.moveEverythingToTruck()
+
+    television.smash()
+
+    expensiveMover.moveEverythingIntoNewPlace()
+    expensiveMover.finishMove()
 }
