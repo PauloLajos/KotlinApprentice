cde6f86d849aa0e9133116833017dd145ab50dfd
PauloLajos <paulolajos@gmail.com>
2023-11-14 15:26:04 +0100

    18. Generics - Maps

--- 18-generics/Chapter_18_Generics/src/main/kotlin/Main.kt
+++ 18-generics/Chapter_18_Generics/src/main/kotlin/Main.kt
@@ -19,4 +19,14 @@ fun main() {
     val things = mutableListOf<Any>(1, 2)
     things.add("Steve")
     println("Things: $things")
+
+    val map = mapOf(
+        Pair("one", 1),
+        Pair("two", "II"),
+        Pair("three", 3.0f)
+    )
+    val valuesForKeysWithE = map.keys
+        .filter { it.contains("e") }
+        .map { "Value for $it: ${map[it]}" }
+    println("Values for keys with E: $valuesForKeysWithE")
 }
